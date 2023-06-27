c478dc55d03d23c508144aa86f3d88d206558d2c
Gabriel Lima <gabrvasco2@gmail.com>
2023-06-27 09:03:16 -0300

    Atualização do balonismo

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Balao b1 = new Balao();
-        Pessoa pessoa = new Pessoa();
+        Pessoa pessoa = new Pessoa("Gabriel", "30", "40", "9", "9", "40", "50");
         Piloto piloto = new Piloto();
 
         String resultado = b1.voar();
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -8,5 +8,13 @@ public class Pessoa {
     String endereco;
     String assinatura;
 
-    public String passear() { return "passeando";}
+    Pessoa (String _nome, String _cpf, String _rg, String _telefone, String _telContato, String _endereco, String _assinatura) {
+        this.nome = _nome;
+        this.cpf = _cpf;
+        this.rg = _rg;
+        this.telefone = _telefone;
+        this.telContato = _telContato;
+        this.endereco = _endereco;
+        this.assinatura = _assinatura;
+    }
 }
